1a3cd7a585302ff3d1baae7b02de1316752568e7
Jared Ramey <rameyjared@gmail.com>
2014-11-04 13:12:03 -0800

    Framework set up
    Everything is laid out for what I need in the library. Now to start
    fleshing out what I have laid out.

---
+++ MyMathLibrary/MyMathLibrary/MyMathLibrary.h
@@ -0,0 +1,52 @@
+//MyMathLibrary.h
+//By: Jared Ramey || Started: 11/4/14
+
+namespace MyMathLibrary
+{
+	class MyMathFunctions
+	{
+	public:
+	//All functions for 2D & 3D Dimensional Vectors
+		//Function for Dot Product
+
+		//Function for Normalization
+
+		//Function for Cross Product
+
+		//Function for Magnitude
+
+		//Function for Operator Overloading
+
+		//Function for Linear Interpolation
+
+
+	//All functions for 3x3 & 4x4 Matrix's
+		//Function for Orthographic Projection Creation
+
+		//Function for Scale
+
+		//Function for Create Rotation
+
+		//Function for Tranform Point
+
+		//Function for Transform Vector
+
+		//Function for Matrix Multiplication
+
+
+	//All functions for Vector 4
+		//Function for Normilization
+
+		//Function for Contruction from Hexadecimal Color Value
+
+
+	//All functions for common math Functionality
+		//Function for Linear Interpolation for Scalar Values
+
+		//Function for Conversion Between degrees and Radians
+
+		//Function for Testing Scalar Value for Power of Two
+
+		//Function for Bitwise Operations
+	};
+}
