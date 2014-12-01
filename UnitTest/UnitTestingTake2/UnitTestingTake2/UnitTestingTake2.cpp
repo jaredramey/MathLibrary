e5704e7adc4ba100f8dae5550186e85f610bd440
Jared Ramey <rameyjared@gmail.com>
2014-12-01 15:11:56 -0800

    Testing
    Testing has started on the library! Now it's time to do all the testing!
    When i'm not too lazy to do it!

--- UnitTest/UnitTestingTake2/UnitTestingTake2/UnitTestingTake2.cpp
+++ UnitTest/UnitTestingTake2/UnitTestingTake2/UnitTestingTake2.cpp
@@ -4,17 +4,34 @@
 #include "MyMathLibrary.h"
 #include "gtest\gtest.h"
 
-using namespace MyMathLibrary;
+using namespace std;
+
+MyMathLibrary::MyBasicMath BasicMath;
+MyMathLibrary::My2DVector Vector2D;
+MyMathLibrary::My3DVector Vector3D;
+MyMathLibrary::My4DVector Vector4D;
+MyMathLibrary::MyMatrix3x3 Matrix3;
+MyMathLibrary::MyMatrix4x4 Matrix4;
 
 TEST(sample_test_case, sample_test)
 {
 	EXPECT_EQ(1, 1);
 }
 
+TEST(Basic_Math_Test, Expected_Result)
+{
+	EXPECT_EQ(BasicMath.DegreeRadianConversion(1, 30), 0);
+}
+
+TEST(Vector2_test, Magnitude)
+{
+	MyMathLibrary::My2DVector NewVector2 = MyMathLibrary::My2DVector::My2DVector(2, 1);
+	EXPECT_FLOAT_EQ(Vector2D.Magnitude(NewVector2), 2.23);
+}
+
 int main(int argc, char** argv)
 {
-	float test1 = 5;
-	float test2 = 3;
+	MyMathLibrary::My2DVector NewVector2 = MyMathLibrary::My2DVector::My2DVector(2, 1);
 
 	testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
